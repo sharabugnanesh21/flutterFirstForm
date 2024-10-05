3b7390c5087dab768cae1747d15a133679ace4e5
Sharabu Gnanesh <sharabugnanesh236@gmail.com>
2024-10-05 16:37:17 +0530

    Add files via upload

---
+++ jsdfklsadj.py
@@ -0,0 +1,20 @@
+str1 = '4201398675'
+str2 = '805'
+c = 0
+str1Pointer = 0
+str2Pointer = 0
+trackSet= set()
+while (str2Pointer<len(str2)):
+  if (str1[str1Pointer] == str2[str2Pointer]):
+    trackSet.add(str1[str1Pointer])
+    str1Pointer+=1
+  else:
+    if (str2[str2Pointer] in trackSet):
+      trackSet.remove(str1[str1Pointer])
+      str1Pointer-=1
+    else:
+      trackSet.add(str1[str1Pointer])
+      str1Pointer+=1
+    c+=1
+    
+print(c)
